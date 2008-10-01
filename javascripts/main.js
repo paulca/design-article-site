25dce94d267b39b0d10eab04db83d7b590af0da7
Paul Campbell <paul@rslw.com>
2008-10-01 18:29:39 +0100

    remove reliance on jQuery, replace with a simple UJS javascript

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,23 @@
-$(function() {
-  $('div#details p').truncate(50,{
-    chars: /\s/,
-    trail: [ " ( <a href='#' class='truncate_show'>full details</a>...)", " (...<a href='#' class='truncate_hide'>less</a> )" ]
-    })
-})
+window.onload = function(){ //load this when the page loads
+    
+  //set up swap variables
+  var summary = document.getElementById('summary') 
+  var long_summary = summary.innerHTML + ' <a href="#" onclick="swap_full_summary();return false;">...less</a>'
+  var short_summary = summary.innerHTML.substr(0,100) + '...' + ' <a href="#" onclick="swap_full_summary();return false;">full details...</a>'
+  var body = document.getElementById('view-page')
+
+
+  //do the swap
+  swap_full_summary = function(){
+  if(summary.innerHTML == long_summary)
+    {
+      summary.innerHTML = short_summary
+    }
+    else
+    {
+      summary.innerHTML = long_summary
+    }
+  }
+
+  summary.innerHTML = short_summary
+}
