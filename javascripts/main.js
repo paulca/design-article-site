314692ac0e56ac2235a40a97b835665371060eb3
Paul Campbell <paul@rslw.com>
2008-10-01 18:34:36 +0100

    update js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,10 @@
 window.onload = function(){ //load this when the page loads
-    
+  
   //set up swap variables
   var summary = document.getElementById('summary') 
   var long_summary = summary.innerHTML + ' <a href="#" onclick="swap_full_summary();return false;">...less</a>'
   var short_summary = summary.innerHTML.substr(0,100) + '...' + ' <a href="#" onclick="swap_full_summary();return false;">full details...</a>'
   var body = document.getElementById('view-page')
-
 
   //do the swap
   swap_full_summary = function(){
@@ -18,6 +17,5 @@ window.onload = function(){ //load this when the page loads
       summary.innerHTML = long_summary
     }
   }
-
   summary.innerHTML = short_summary
 }
